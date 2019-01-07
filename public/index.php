68dd6be8e3cbf2d8de4b712503c24cdff7d52b4c
Andrew Alyamovsky <ddlzz@yandex.ru>
2019-01-07 18:15:47 +0300

    added code

--- public/index.php
+++ public/index.php
@@ -1,6 +1,16 @@
 <?php
 
-$name = $_GET['name'] ?? 'vasya';
+declare(strict_types=1);
+
+use Framework\Http\Request;
+
+require __DIR__.'/../vendor/autoload.php';
+
+// init
+
+$request = new Request();
+
+$name = $request->getQueryParams()['name'] ?? 'vasya';
 
 \header('X-Developer: ddlzz');
 echo \sprintf('hello, %s!', $name);
