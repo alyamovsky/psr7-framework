06504a8e8a96f77ca279c04a4306b039516117ac
Andrew Alyamovsky <ddlzz@yandex.ru>
2019-01-08 16:47:15 +0300

    added zend http handler

--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-use Framework\Http\ResponseSender;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\ServerRequestFactory;
+use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
 require __DIR__.'/../vendor/autoload.php';
 
@@ -20,7 +20,7 @@ $response = (new HtmlResponse(\sprintf('hello, %s!', $name)))
     ->withHeader('X-Developer', 'ddlzz')
 ;
 
-// render
+// send
 
-$emitter = new ResponseSender();
-$emitter->send($response);
+$emitter = new SapiEmitter();
+$emitter->emit($response);
