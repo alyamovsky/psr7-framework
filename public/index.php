4ac9c1b0f635a923fe47741a8ba0b634a66a0d9b
Andrew Alyamovsky <ddlzz@yandex.ru>
2019-01-08 23:30:37 +0300

    updated index

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\Response\JsonResponse;
 use Zend\Diactoros\ServerRequestFactory;
 use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 
@@ -27,8 +28,20 @@ if ('/' === $path) {
     $response = new HtmlResponse(\sprintf('hello, %s!', $name));
 } elseif ('/about' === $path) {
     $response = new HtmlResponse('I am a simple site.');
+} elseif ('/blog' === $path) {
+    $response = new JsonResponse([
+        ['id' => 2, 'title' => 'The second post'],
+        ['id' => 1, 'title' => 'The first post'],
+    ]);
+} elseif (\preg_match('~^/blog/(?P<id>\d+)$~i', $path, $matches)) {
+    $id = $matches['id'];
+    if ($id > 2) {
+        $response = new JsonResponse(['error' => 'Undefined page'], 404);
+    } else {
+        $response = new JsonResponse(['id' => $id, 'title' => \sprintf('Post #%d', $id)]);
+    }
 } else {
-    $response = new \Zend\Diactoros\Response\JsonResponse(['error' => 'Undefined page', 404]);
+    $response = new JsonResponse(['error' => 'Undefined page', 404]);
 }
 
 // postprocessing
